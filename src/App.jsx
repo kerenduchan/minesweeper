e0df708208f145080a3049e381b98d3d23085624
Keren Duchan <keren.duchan@gmail.com>
2024-05-05 18:51:08 +0300

    sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 export function App() {
     return (
         <div>
-            <img src="public/n-0.svg" />
-            <img src="public/n-1.svg" />
-            <img src="public/n-2.svg" />
-            <img src="public/n-3.svg" />
-            <img src="public/n-4.svg" />
-            <img src="public/n-5.svg" />
-            <img src="public/n-6.svg" />
-            <img src="public/n-7.svg" />
-            <img src="public/n-8.svg" />
+            <img src="/n-0.svg" />
+            <img src="/n-1.svg" />
+            <img src="/n-2.svg" />
+            <img src="/n-3.svg" />
+            <img src="/n-4.svg" />
+            <img src="/n-5.svg" />
+            <img src="/n-6.svg" />
+            <img src="/n-7.svg" />
+            <img src="/n-8.svg" />
         </div>
     )
 }
