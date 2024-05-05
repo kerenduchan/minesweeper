abd1965f31ea0fca63aeba8a4c2aca013b656c03
Keren Duchan <keren.duchan@gmail.com>
2024-05-05 21:19:03 +0300

    wip styling: flag svg

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Board } from './cmps/Board'
 export function App() {
     let board = [
         ['00', '01', '02', '03', '04', '05', '06', '07', '08'],
-        ['bb', 'bx', 'br', 'un', 'un', '00', '00', '00', '00'],
+        ['bb', 'bx', 'br', 'un', 'un', 'fl', '00', '00', '00'],
     ]
     return <Board board={board} />
 }
