343f7feb2cfdbedbdb6429ab9ed3e46936f5bacd
Keren Duchan <keren.duchan@gmail.com>
2024-05-05 18:42:57 +0300

    styling: number svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
 export function App() {
-    return <div>hi</div>
+    return (
+        <div>
+            <img src="public/n-0.svg" />
+            <img src="public/n-1.svg" />
+            <img src="public/n-2.svg" />
+            <img src="public/n-3.svg" />
+            <img src="public/n-4.svg" />
+            <img src="public/n-5.svg" />
+            <img src="public/n-6.svg" />
+            <img src="public/n-7.svg" />
+            <img src="public/n-8.svg" />
+        </div>
+    )
 }
