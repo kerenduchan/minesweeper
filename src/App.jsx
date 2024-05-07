1262101b371b119e4db4ef86dadb8da228079803
Keren Duchan <keren.duchan@gmail.com>
2024-05-07 17:25:04 +0300

    refactor: Game component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,5 @@
-import { useState } from 'react'
-import { gameService } from './services/game.service'
-import { Board } from './cmps/Board'
+import { Game } from './cmps/Game'
 
 export function App() {
-    const [game, setGame] = useState(gameService.getGame())
-
-    function onMouseDown(rowIdx, colIdx) {
-        setCellMouseDown([rowIdx, colIdx])
-    }
-
-    function onMouseOver(rowIdx, colIdx) {
-        if (game.cellMouseDown) {
-            setCellMouseDown([rowIdx, colIdx])
-        }
-    }
-
-    function onMouseUp(rowIdx, colIdx) {
-        const newGame = gameService.exposeCell(rowIdx, colIdx)
-        setGame(newGame)
-    }
-
-    function setCellMouseDown(val) {
-        const newGame = gameService.setCellMouseDown(val)
-        setGame(newGame)
-    }
-
-    return (
-        <Board
-            game={game}
-            onMouseDown={onMouseDown}
-            onMouseOver={onMouseOver}
-            onMouseUp={onMouseUp}
-        />
-    )
+    return <Game />
 }
---
+++ src/cmps/Game.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { gameService } from '../services/game.service'
+import { Board } from './Board'
+
+export function Game() {
+    const [game, setGame] = useState(gameService.getGame())
+
+    function onMouseDown(rowIdx, colIdx) {
+        setCellMouseDown([rowIdx, colIdx])
+    }
+
+    function onMouseOver(rowIdx, colIdx) {
+        if (game.cellMouseDown) {
+            setCellMouseDown([rowIdx, colIdx])
+        }
+    }
+
+    function onMouseUp(rowIdx, colIdx) {
+        const newGame = gameService.exposeCell(rowIdx, colIdx)
+        setGame(newGame)
+    }
+
+    function setCellMouseDown(val) {
+        const newGame = gameService.setCellMouseDown(val)
+        setGame(newGame)
+    }
+
+    return (
+        <Board
+            game={game}
+            onMouseDown={onMouseDown}
+            onMouseOver={onMouseOver}
+            onMouseUp={onMouseUp}
+        />
+    )
+}
