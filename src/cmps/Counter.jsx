16714324a33c60184b113be9a0c0c0277a6f4bdd
Keren Duchan <keren.duchan@gmail.com>
2024-05-15 13:07:20 +0300

    counter

---
+++ src/cmps/Counter.jsx
@@ -0,0 +1,25 @@
+import { BevelledBox } from './BevelledBox'
+
+export function Counter({ value, className = '' }) {
+    function getDigitSvg(idx) {
+        let valueStr = String(Math.abs(value)).padStart(2, '0')
+
+        if (value < 0) {
+            valueStr = '-' + valueStr
+        } else if (value <= 99) {
+            valueStr = '0' + valueStr
+        }
+        const digit = valueStr[idx]
+        return `counters/${digit}.svg`
+    }
+
+    return (
+        <div className={`counter ${className}`}>
+            <BevelledBox isInset={true} bevelWidth={1}>
+                {[0, 1, 2].map((idx) => (
+                    <img className="digit-img" src={getDigitSvg(idx)} />
+                ))}
+            </BevelledBox>
+        </div>
+    )
+}
